f45d69676878751344dba2e53dc5d30dbf732eee
Marc-Antoine Ferland <maferland@hey.com>
2020-11-17 18:32:36 -0500

    :bug: Missing export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export {default as useInterval} from './use-interval'
 export {default as useLocalStorageState} from './use-local-storage-state'
 export {default as useScrollDirection} from './use-scroll-direction'
+export {ScrollDirection} from './use-scroll-direction'
